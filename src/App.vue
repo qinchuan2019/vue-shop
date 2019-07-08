ac82e466c028a00a91a19a8031bc4440d6d3201c
qinchuan <317423553@qq.com>
2019-07-08 19:56:23 +0800

    添加了一些文件

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="app-container">
+        <!-- Header区域 -->
+        <mt-header fixed title="vue-shop"></mt-header>
+
+        <!-- 底部TabBar区域 -->
+        <nav class="mui-bar mui-bar-tab">
+            <a class="mui-tab-item m-ui-active">
+                <span class="mui-icon mui-icon-home"></span>
+                <span class="mui-tab-label">首页</span>
+            </a>
+            <a class="mui-tab-item">
+                <span class="mui-icon mui-icon-contact"></span>
+                <span class="mui-tab-label">会员</span>
+            </a>
+            <a class="mui-tab-item">
+                <span class="mui-icon mui-icon-extra mui-icon-extra-cart">
+                    <span class="mui-badge">0</span>
+                </span>
+                <span class="mui-tab-label">购物车</span>
+            </a>
+            <a class="mui-tab-item">
+                <span class="mui-icon mui-icon-search"></span>
+                <span class="mui-tab-label">搜索</span>
+            </a>
+        </nav>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+.app-container{
+    padding-top: 40px;
+}
+</style>
+
+
+
---
+++ src/router.config.js
@@ -0,0 +1,6 @@
+var router = new VueRouter({
+  routes:[   
+  ]
+})
+
+export default router;
