766ed9f512a5d0478739eda9039a84c79d2bd30c
qinchuan2019 <317423553@qq.com>
2019-07-09 18:06:51 +0800

    dddd

--- src/components/tabBar/HomeContainer.vue
+++ src/components/tabBar/HomeContainer.vue
@@ -1,15 +1,62 @@
 <template>
     <div>
-        <h2>HomeContainer</h2>
+        <mt-swipe :auto="3000">
+            <mt-swipe-item>1</mt-swipe-item>
+            <mt-swipe-item>2</mt-swipe-item>
+            <mt-swipe-item>3</mt-swipe-item>
+        </mt-swipe>
     </div>
 </template>
 
 <script>
+
+import Mock from '../../lib/mock/mock.js'
+import Toast from 'mint-ui'
+
+Mock.mock('getList',{
+    "code": 0,
+    "message": "请求成功",
+    "data|3": [{
+        'image':'@image(300x200)'
+    }]
+})
+
 export default {
-    
+    data(){
+        return {
+            lunbotuList:[]
+        }
+    },
+    methods:{
+        getLunbotu(){
+            this.$http.get('getList').then( result => {
+                if(result.body.code === 0){
+                    alert(Toast)
+                }else{
+                    
+                }
+            })
+        }
+    },
+    created(){
+        this.getLunbotu()
+    }
 }
 </script>
 
-<style lang="sass" scoped>
-
+<style lang="scss" scoped>
+.mint-swipe{
+    height: 200px;
+    .mint-swipe-item{
+        &:nth-child(1){
+            background-color: red;
+        }
+        &:nth-child(2){
+            background-color: yellow;
+        }
+        &:nth-child(3){
+            background-color: cyan;
+        }
+    }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,13 +3,17 @@
  */
 import Vue from 'vue'
 import app from './App.vue'
-import {Header} from 'mint-ui'
+import {Header,Swipe,SwipeItem} from 'mint-ui'
 import VueRouter from 'vue-router'
 import router from './router.config.js'
+import VueResource from 'vue-resource'
 
 Vue.use(VueRouter)
+Vue.use(VueResource)
 
 Vue.component(Header.name,Header)
+Vue.component(Swipe.name,Swipe)
+Vue.component(SwipeItem.name,SwipeItem)
 
 import './lib/mui/css/mui.min.css'
 import './lib/mui/css/icons-extra.css'
